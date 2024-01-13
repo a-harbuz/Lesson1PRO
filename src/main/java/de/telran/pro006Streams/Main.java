7ca1d0c3048a2c46d2e11fd94b8df57636fd3a2b
pamail08@gmail.com <pamail08@gmail.com>
2024-01-13 22:37:56 +0100

    Challenge 2

--- src/main/java/de/telran/pro006Streams/Main.java
+++ src/main/java/de/telran/pro006Streams/Main.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
@@ -33,6 +34,11 @@ public class Main {
 
         System.out.println(s);
 
+        IntStream.of(50, 60, 70, 80, 90, 100, 110, 120)
+                .filter(x -> x < 90)
+                .map(x -> x + 10)
+                .limit(3)
+                .forEach(System.out::println);
     }
 
 
